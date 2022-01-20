535d5a6ff3769ea215ce343e314450508528918a
Ed Hickey <erhickey@gmail.com>
2022-01-19 23:24:40 -0500

    updates

--- .bash_autoload/prompt.sh
+++ .bash_autoload/prompt.sh
@@ -12,31 +12,32 @@ post_command_exec() {
   unset COMMAND_EXECUTING
 
   COMMAND_END="$(date +%s%3N)"
-  COMMAND_DURATION_DISPLAY="0.000s"
+  COMMAND_DURATION_DISPLAY="0.000"
   # skip if date did not give desired output, e.g., when macos date util is used
   if [[ "$COMMAND_END" != *N ]] && [[ "$COMMAND_START" != *N ]] ; then
     COMMAND_DURATION="$((COMMAND_END - COMMAND_START))"
     CD_SECS="$((COMMAND_DURATION / 1000))"
     CD_MILLIS="$((COMMAND_DURATION - CD_SECS * 1000))"
-    COMMAND_DURATION_DISPLAY="$CD_SECS.$(printf '%03d' "$CD_MILLIS")s"
+    COMMAND_DURATION_DISPLAY="$CD_SECS.$(printf '%03d' "$CD_MILLIS")"
   fi
+  COMMAND_DURATION_DISPLAY="${COMMAND_DURATION_DISPLAY}s"
 }
 
 trap pre_command_exec DEBUG
 
 set_error_prompt() {
   if [ $? -eq 0 ] ; then
-    PS1_ERROR_PROMPT=""
+    unset PS1_ERROR_PROMPT
   else
-    PS1_ERROR_PROMPT="$(printf '\e[38;5;3m\e[48;5;1m%s\e[38;5;8m\e[48;5;1m%s\e[38;5;1m\e[48;5;8m%s' " " "   $? " "")"
+    PS1_ERROR_PROMPT=" $?"
   fi
 }
 
 set_git_branch() {
   if cbranch="$(git symbolic-ref --short HEAD 2> /dev/null)"; then
-    PS1_GIT_BRANCH="$(printf '\e[38;5;4m\e[48;5;130m%s\e[38;5;8m\e[48;5;130m%s\e[38;5;130m\e[48;5;8m%s' "" "  $cbranch " "")"
+    PS1_GIT_BRANCH="  $cbranch"
   else
-    PS1_GIT_BRANCH=""
+    unset PS1_GIT_BRANCH
   fi
 }
 
@@ -51,6 +52,6 @@ prompt_command() {
   set_git_branch
 }
 
-PS1="\\[$BG_BLUE\\]\\[$FG_REVERSE\] \\w\\[$FG_BLUE\\]\\[$BG_RESET\\]$(printf '$PS1_GIT_BRANCH')\\[$COLOR_RESET\\]\\n\\[$BG_YELLOW\\]\\[$FG_REVERSE\\] $(printf '$COMMAND_DURATION_DISPLAY') \\[$BG_RESET\\]\\[$FG_YELLOW\\]$(printf '$PS1_ERROR_PROMPT')\\[$COLOR_RESET\\] "
+PS1="\\[$FG_BLUE\]\\w\\[$FG_YELLOW\\]$(printf "%s" '$PS1_GIT_BRANCH')\\[$FG_RED\\]$(printf "%s" '$PS1_ERROR_PROMPT')\\[$COLOR_RESET\\]\\n\\[$FG_ORANGE\\]$(printf "%s" '$COMMAND_DURATION_DISPLAY')\\[$COLOR_RESET\\]"
 
 PROMPT_COMMAND=prompt_command
