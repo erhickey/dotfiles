b30ee1083a87b3e2ab92c62cdaab44c77d1837e4
Ed Hickey <erhickey@gmail.com>
2022-09-25 16:30:54 -0400

    updates

--- .config/nvim/lua/autorun/a_vim-plug.lua
+++ .config/nvim/lua/autorun/a_vim-plug.lua
@@ -2,58 +2,72 @@ local Plug = vim.fn['plug#']
 
 vim.call('plug#begin')
 
-    Plug 'erhickey/diagline-nvim'
-    Plug 'erhickey/buffer-command-vim'
-
+    -- ----------------------
+    -- LSP
+    -- ----------------------
     Plug 'neovim/nvim-lspconfig'
-
-    Plug 'editorconfig/editorconfig-vim'
-
-    Plug 'junegunn/fzf'
-    Plug 'junegunn/fzf.vim'
-
-    Plug 'tpope/vim-fugitive'
-    Plug 'tpope/vim-commentary'
-
-    Plug 'mattn/emmet-vim'
-
-    -- nvim-cmp and supporting sources
-    Plug 'hrsh7th/cmp-nvim-lsp'
-    Plug 'hrsh7th/cmp-vsnip'
-    Plug 'hrsh7th/cmp-buffer'
-    Plug 'hrsh7th/cmp-path'
-    Plug 'hrsh7th/nvim-cmp'
-
-    -- vsnip, a snippet engine is required by nvim-cmp
-    Plug 'hrsh7th/vim-vsnip'
-
-    -- required to add parens after nvim-cmp function completion
-    Plug 'windwp/nvim-autopairs'
-
-    -- colored icons, required by other plugins
-    Plug 'kyazdani42/nvim-web-devicons'
-
     -- identify lsp kinds with text/icon, requires nerdfont
     Plug 'onsails/lspkind-nvim'
-
     -- display function signature popup
     Plug 'ray-x/lsp_signature.nvim'
 
-    -- indicate modified lines in VCS managed files
-    Plug 'mhinz/vim-signify'
+    -- ----------------------
+    -- treesitter
+    -- ----------------------
+    Plug('nvim-treesitter/nvim-treesitter', { ['do'] = ':TSUpdate' })
+    Plug 'nvim-treesitter/nvim-treesitter-context'
+    -- make comment string context aware
+    Plug 'JoosepAlviste/nvim-ts-context-commentstring'
+    -- rainbow colored pairs
+    Plug 'p00f/nvim-ts-rainbow'
 
-    -- better quickfix window
-    Plug 'kevinhwang91/nvim-bqf'
+    -- ----------------------
+    -- git
+    -- ----------------------
+    Plug 'tpope/vim-fugitive'
+    Plug 'lewis6991/gitsigns.nvim'
 
-    -- create closing html/xml tag when completing opening tag
+    -- ----------------------
+    -- markup language
+    -- ----------------------
+    Plug 'mattn/emmet-vim'
     Plug 'alvan/vim-closetag'
     -- auto-update matching html/xml tag
     Plug 'AndrewRadev/tagalong.vim'
 
+    -- ----------------------
+    -- nvim-cmp
+    -- ----------------------
+    Plug 'hrsh7th/cmp-nvim-lsp'
+    -- supporting sources
+    Plug 'hrsh7th/cmp-vsnip'
+    Plug 'hrsh7th/cmp-buffer'
+    Plug 'hrsh7th/cmp-path'
+    Plug 'hrsh7th/nvim-cmp'
+    -- required to add parens after nvim-cmp function completion
+    Plug 'windwp/nvim-autopairs'
+    -- snippet engine is required by nvim-cmp
+    Plug 'hrsh7th/vim-vsnip'
+
+    -- ----------------------
+    -- SQL
+    -- ----------------------
+    Plug 'ivalkeen/vim-simpledb'
+
+    -- ----------------------
+    -- miscellaneous
+    -- ----------------------
+    Plug 'editorconfig/editorconfig-vim'
+    Plug 'tpope/vim-commentary'
+    Plug 'junegunn/fzf'
+    Plug 'junegunn/fzf.vim'
+    Plug 'erhickey/buffer-command-vim'
+    Plug 'erhickey/diagline-nvim'
+    -- better quickfix window
+    Plug 'kevinhwang91/nvim-bqf'
     -- easily jump to arbitraty point in view
     Plug 'easymotion/vim-easymotion'
-
-    -- simple database client plugin
-    Plug 'ivalkeen/vim-simpledb'
+    -- colored icons, required by other plugins
+    Plug 'kyazdani42/nvim-web-devicons'
 
 vim.call('plug#end')
---
+++ .config/nvim/lua/autorun/treesitter.lua
@@ -0,0 +1,38 @@
+local rainbow_enabled_list = {"clojure", "fennel", "commonlisp", "query"}
+
+require('nvim-treesitter.configs').setup({
+  ensure_installed = 'all',
+  sync_install = false,
+  auto_install = true,
+  ignore_install = { 'phpdoc' },
+  highlight = {
+    enable = true,
+    disable = {},
+    -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
+    -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
+    -- Using this option may slow down your editor, and you may see some duplicate highlights.
+    -- Instead of true it can also be a list of languages
+    additional_vim_regex_highlighting = false,
+  },
+  indent = {
+    enable = true
+  },
+  rainbow = {
+    enable = true,
+    extended_mode = true, -- Also highlight non-bracket delimiters like html tags, boolean or table: lang -> boolean
+    max_file_lines = nil, -- Do not enable for files with more than n lines, int
+   -- Enable only for lisp like languages
+    disable = vim.tbl_filter(
+      function(p)
+        for _, lang in pairs(rainbow_enabled_list) do
+          if p==lang then return false end
+        end
+        return true
+      end,
+      require('nvim-treesitter.parsers').available_parsers()
+    )
+  },
+  context_commentstring = {
+    enable = true,
+  },
+})
