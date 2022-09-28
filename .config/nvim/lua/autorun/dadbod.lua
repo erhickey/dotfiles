87563415e7cb86c51b865fced298c48c57d250a5
Ed Hickey <erhickey@gmail.com>
2022-09-28 13:15:26 -0400

    updates

--- .config/nvim/lua/autorun/dadbod.lua
+++ .config/nvim/lua/autorun/dadbod.lua
@@ -1,33 +1,19 @@
-local function dadbod_get_conn_string()
-  local url = vim.fn.getline(1)
-  return string.sub(url, 3)
-end
-
-local function dadbod_get_query(start_line, end_line)
-  local query = vim.fn.getline(start_line, end_line)
-  return table.concat(query, ' ')
-end
-
-function dadbod_execute_query(start_line, end_line)
-  vim.api.nvim_command(
-    string.format(
-      '%s %s %s',
-      'DB ',
-      dadbod_get_conn_string(),
-      dadbod_get_query(start_line, end_line)
-    )
-  )
-end
-
 vim.api.nvim_create_user_command(
-  'DadbodExecuteSql',
+  'DBExec',
   function(opts)
-    dadbod_execute_query(opts.line1, opts.line2)
+    vim.api.nvim_command(
+      string.format(
+        '%s,%sDB %s',
+        opts.line1,
+        opts.line2,
+        string.sub(vim.fn.getline(1), 3)
+      )
+    )
   end,
   { range = true }
 )
 
 local opts = { expr=false, noremap=true, silent=true }
-vim.api.nvim_set_keymap('v', '<CR>', ':DadbodExecuteSql<CR>', opts);
-vim.api.nvim_set_keymap('n', '<leader><CR>', "m':'{,'}DadbodExecuteSql<CR>g`'", opts);
-vim.api.nvim_set_keymap('i', '<C-B>', "<Esc>m':'{,'}DadbodExecuteSql<CR>g`'a", opts);
+vim.api.nvim_set_keymap('v', '<CR>', ':DBExec<CR>', opts);
+vim.api.nvim_set_keymap('n', '<leader><CR>', "m':'{,'}DBExec<CR>g`'", opts);
+vim.api.nvim_set_keymap('i', '<C-B>', "<Esc>m':'{,'}DBExec<CR>g`'a", opts);
