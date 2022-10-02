ee364e3aee72f1100a04c60a07650424ea86e406
Ed Hickey <erhickey@gmail.com>
2022-10-02 12:27:20 -0400

    updates

--- .config/nvim/lua/autorun/lsp_signature.lua
+++ .config/nvim/lua/autorun/lsp_signature.lua
@@ -1,4 +1,9 @@
 require('lsp_signature').setup({
+  max_height = 20,
+  max_width = 100,
   floating_window_above_cur_line = false,
+  floating_window_off_x = 0,
   hint_enable = false,
+  toggle_key = '<M-k>',
+  select_signature_key = '<M-tab>',
 })
