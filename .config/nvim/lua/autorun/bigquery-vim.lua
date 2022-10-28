12af1f1b9ac20634170604348cf820c0e1213f5f
Ed Hickey <erhickey@gmail.com>
2022-10-28 10:29:35 -0400

    updates

--- .config/nvim/lua/autorun/bigquery-vim.lua
+++ .config/nvim/lua/autorun/bigquery-vim.lua
@@ -1,2 +1,11 @@
-local opts = { noremap=true, silent=true }
-vim.api.nvim_set_keymap('n', '<leader><CR>', "m'vap:BQExecute<CR>g`'", opts);
+local function create_keymaps()
+  local opts = { noremap=true, silent=true }
+  vim.api.nvim_set_keymap('n', '<leader><CR>', "m'vap:BQExecute<CR>g`'", opts);
+end
+
+local au_group_name = 'bigquery_vim_aug'
+vim.api.nvim_create_augroup(au_group_name, {clear = true})
+vim.api.nvim_create_autocmd(
+  { 'BufReadPost', 'BufNewFile' },
+  { callback = create_keymaps, group = au_group_name, pattern = { '*.bq' } }
+)
