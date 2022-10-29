95b65d4f6ba8701d81dae18dcf7efc26d58b2bc3
Ed Hickey <erhickey@gmail.com>
2022-10-28 20:35:41 -0400

    updates

--- .config/nvim/lua/autorun/diagline.lua
+++
@@ -1,1 +0,0 @@
-require('diagline-nvim').setup()
---
+++ .config/nvim/lua/autorun/diagnostics.lua
@@ -0,0 +1,43 @@
+local severities = {
+  Error = {
+    icon = '',
+    fg = '#cc241d'
+  },
+  Warn = {
+    icon = '',
+    fg = '#d65d0e'
+  },
+  Hint = {
+    icon = '',
+    fg = '#458588'
+  },
+  Info = {
+    icon = '',
+    fg = '#a89984'
+  }
+}
+
+for type, config in pairs(severities) do
+  vim.cmd('highlight clear Diagnostic' .. type)
+  vim.cmd('highlight Diagnostic' .. type .. ' guifg=' .. config.fg)
+
+  vim.fn.sign_define(
+    'DiagnosticSign' .. type,
+    {
+      text = config.icon,
+      texthl = 'Diagnostic' .. type,
+      numhl = 'Diagnostic' .. type,
+    })
+end
+
+vim.diagnostic.config({
+  severity_sort = true,
+  float = {
+    border = 'rounded',
+    scope = 'cursor',
+    severity_sort = true,
+    source = true,
+  },
+})
+
+require('diagline-nvim').setup()
