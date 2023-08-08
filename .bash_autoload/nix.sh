2c0a64c255aae7273e870b15e0827e63d8790632
Eddie Hickey <erhickey@gmail.com>
2023-08-08 11:54:18 -0400

    updates

--- .bash_autoload/nix.sh
+++ .bash_autoload/nix.sh
@@ -2,3 +2,6 @@
 
 # in case nix was installed for single user
 [ -e "$HOME"/.nix-profile/etc/profile.d/nix.sh ] && source "$HOME"/.nix-profile/etc/profile.d/nix.sh
+
+# may be necessary to point to system's locale archive
+[ -e /usr/lib/locale/locale-archive ] && export LOCALE_ARCHIVE=/usr/lib/locale/locale-archive
