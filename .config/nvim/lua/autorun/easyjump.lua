e6600941d508b80bdada9216ac035a68efbe84cd
Eddie Hickey <erhickey@gmail.com>
2023-08-21 12:19:27 -0400

    updates

---
+++ .config/nvim/lua/autorun/easyjump.lua
@@ -0,0 +1,4 @@
+local wk = require('which-key')
+
+wk.register({ u = { '<Plug>EasyJump', 'EasyJump' } }, { prefix = '<leader>' })
+wk.register({ u = { '<Plug>EasyJump', 'EasyJump' } }, { prefix = '<leader>', mode = 'v' })
