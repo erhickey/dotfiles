d9c496641ef520aebe96e311c19eeeab5eb45bb3
Eddie Hickey <erhickey@gmail.com>
2023-10-08 13:15:18 -0400

    updates

--- .bash_autoload/git.sh
+++ .bash_autoload/git.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
 
 alias gitlog='git log --graph --pretty=format:"%Cred%H%Creset -%C(yellow)%d%Creset %s %Cgreen(%cD) (%cr) %C(bold blue)<%an>%Creset"'
+
+[ -e ~/.bash_autoload/git-completion.bash ] || curl -fLso ~/.bash_autoload/git-completion.bash https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash && source ~/.bash_autoload/git-completion.bash
